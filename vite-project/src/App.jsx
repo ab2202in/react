93b96fc4a6cfedf549398d5a11781c520adca756
abin sasikumar <abinsasikumar22@gmail.com>
2024-02-16 09:33:09 +0530

    react

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+
+function App() {
+
+  return (
+    <>
+    <center>
+ <h2>star anwarsha</h2>
+ <h4>in & as</h4>
+      <h1>hello era2petta</h1>
+     
+    </center>
+    </>
+  )
+}
+
+export default App
